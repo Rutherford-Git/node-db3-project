21dc3a8483fd796119c4341d310c6e943d960c13
Rutherford Alexis <rutherford.aalexis@Gmail.com>
2022-10-09 18:08:02 -0400

    done

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -6,8 +6,16 @@
     "message": "scheme with scheme_id <actual id> not found"
   }
 */
-const checkSchemeId = (req, res, next) => {
+const db = require('../../data/db-config.js')
 
+const checkSchemeId = async (req, res, next) => {
+ // const {scheme_id} = req.params
+  const id = await db('schemes').where('scheme_id', req.params.scheme_id).first()
+  if (id) {
+    next()
+  } else {
+    next({ message: `scheme with scheme_id ${req.params.scheme_id} not found`, status: 404})
+  }
 }
 
 /*
@@ -19,7 +27,16 @@ const checkSchemeId = (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
-
+  const {scheme_name } = req.body
+  if (
+    scheme_name === undefined ||
+    typeof scheme_name !== 'string' ||
+    !scheme_name.trim()
+  ){
+    next({status: 400, message: 'invalid scheme_name'})
+  } else {
+    next()
+  }
 }
 
 /*
@@ -32,7 +49,18 @@ const validateScheme = (req, res, next) => {
   }
 */
 const validateStep = (req, res, next) => {
-
+  const {instructions, step_number } = req.body
+  if (
+    instructions === undefined ||
+    typeof instructions !== 'string' ||
+    !instructions.trim() ||
+    typeof step_number !== 'number' ||
+    step_number < 1
+  ){
+    next({status: 400, message: 'invalid step'})
+  } else {
+    next()
+  }
 }
 
 module.exports = {
